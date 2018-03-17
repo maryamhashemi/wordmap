61fc9321e11e1f35510ae56540afafdd6ad289f2
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2018-03-17 13:22:26 +0330

    Load politics and spotrs News & omit preposition

---
+++ wordmap/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace wordmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(193, 537);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(264, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(193, 537);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(512, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(193, 537);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 586);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ wordmap/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wordmap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load list of persian Preposition 
+            string[] prepositionLines = System.IO.File.ReadAllLines(@"C:\Users\maryam\Desktop\preposition.txt");
+
+            //Preposition Data Table
+            DataTable preposition = new DataTable();
+            preposition.Columns.Add("preposition", typeof(string));
+
+            foreach (string line in prepositionLines)
+            {
+                DataRow dr = preposition.NewRow();
+                dr["preposition"] = line;
+                preposition.Rows.Add(dr);
+            }
+            //Display preposition in dataGrid 
+            dataGridView1.DataSource = preposition;
+
+            //Load politics News
+            string[] politicsLines = System.IO.File.ReadAllLines(@"C:\Users\maryam\Desktop\politicsNews .txt");
+
+            //politicswords Data Table
+            DataTable politicsWords = new DataTable();
+            politicsWords.Columns.Add("word", typeof(string));
+
+            bool isPreposition;
+            foreach (string line in politicsLines)
+            {
+                string[] words = line.Split(new string[] { " ", ":", "/", ".", "؛", "،", "-" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    //Omit preposition from politics word
+                    isPreposition = false;
+                    foreach (DataRow row in preposition.Rows)
+                    {
+                        if(row.Field<string>("preposition") == word)
+                        {
+                            isPreposition = true;
+                            break;
+                        }
+                    }
+                    if(isPreposition == false)
+                    {
+                        DataRow dr = politicsWords.NewRow();
+                        dr["word"] = word;
+                        politicsWords.Rows.Add(dr);
+                    }  
+                }
+            }
+            //Display politicsWords in dataGrid 
+            dataGridView2.DataSource = politicsWords;
+
+            //Load sports News file
+            string[] sportsLines = System.IO.File.ReadAllLines(@"C:\Users\maryam\Desktop\sportsNews.txt");
+
+            //sportsWords Data Table
+            DataTable sportsWords = new DataTable();
+            sportsWords.Columns.Add("word", typeof(string));
+
+
+            foreach (string line in sportsLines)
+            {
+                string[] words = line.Split(new string[] { " ", ":", "/", ".", "؛", "،","-" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    //Omit preposition from politics word
+                    isPreposition = false;
+                    foreach (DataRow row in preposition.Rows)
+                    {
+                        if (row.Field<string>("preposition") == word)
+                        {
+                            isPreposition = true;
+                            break;
+                        }
+                    }
+                    if (isPreposition == false)
+                    {
+                        DataRow dr = sportsWords.NewRow();
+                        dr["word"] = word;
+                        sportsWords.Rows.Add(dr);
+                    }
+                }
+
+            }
+            //Display sportsWords in dataGrid 
+            dataGridView3.DataSource = sportsWords;
+
+            
+        }
+    }
+}
