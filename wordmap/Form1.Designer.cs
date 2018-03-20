7c513a4d3b8302923895c8351e5b5cae7bbc868f
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2018-03-20 17:40:15 +0330

    add sentences and UI

--- wordmap/Form1.Designer.cs
+++ wordmap/Form1.Designer.cs
@@ -28,64 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.button1.Font = new System.Drawing.Font("Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(269, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 73);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "خبر ورزشی ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.button2.Font = new System.Drawing.Font("Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(618, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "خبر سیاسی ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nazanin", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(295, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(574, 178);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "به برنامه واژه نما خوش آمدید\r\n\r\n";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.Font = new System.Drawing.Font("Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button3.Location = new System.Drawing.Point(27, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "برگشت";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(88, 68);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(193, 537);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(781, 596);
+            this.dataGridView1.TabIndex = 5;
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(239, 12);
+            this.dataGridView2.Location = new System.Drawing.Point(892, 68);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(304, 537);
-            this.dataGridView2.TabIndex = 0;
-            // 
-            // dataGridView3
-            // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(569, 12);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(308, 537);
-            this.dataGridView3.TabIndex = 0;
+            this.dataGridView2.Size = new System.Drawing.Size(233, 596);
+            this.dataGridView2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 586);
-            this.Controls.Add(this.dataGridView3);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "wordmap";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
 
--- wordmap/Form1.cs
+++ wordmap/Form1.cs
@@ -8,26 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using NHazm;
 
 namespace wordmap
 {
     public partial class Form1 : Form
     {
+        //Preposition Data Table
+        DataTable preposition = new DataTable();
+
+        //verb Data Table
+        DataTable verb = new DataTable();
+        
         public Form1()
         {
             InitializeComponent();
+
+            preposition.Columns.Add("preposition", typeof(string));
+            verb.Columns.Add("verb", typeof(string));
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            button3.Visible = false;
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = false;
+
             //Load list of persian Preposition 
             string currentDirectory = Directory.GetCurrentDirectory();
             string filePath = Path.Combine(currentDirectory, "preposition.txt");
-            string[] prepositionLines = System.IO.File.ReadAllLines(filePath);
-
-            //Preposition Data Table
-            DataTable preposition = new DataTable();
-            preposition.Columns.Add("preposition", typeof(string));
+            string[] prepositionLines = File.ReadAllLines(filePath);
 
             foreach (string line in prepositionLines)
             {
@@ -35,109 +45,67 @@ namespace wordmap
                 dr["preposition"] = line;
                 preposition.Rows.Add(dr);
             }
-            //Display preposition in dataGrid 
-            dataGridView1.DataSource = preposition;
-
 
             //Load list of persian verbs
             filePath = Path.Combine(currentDirectory, "verb.txt");
-            string[] verbLines = System.IO.File.ReadAllLines(filePath);
-
-            //verb Data Table
-            DataTable verb = new DataTable();
-            verb.Columns.Add("verb", typeof(string));
-
+            string[] verbLines = File.ReadAllLines(filePath);
+        
             foreach (string line in verbLines)
             {
                 DataRow dr = verb.NewRow();
                 dr["verb"] = line;
                 verb.Rows.Add(dr);
-            }
+            }            
+        }
 
-            //Load politics News
-            filePath = Path.Combine(currentDirectory, "politicsNews .txt");
-            string[] politicsLines = System.IO.File.ReadAllLines(@"C:\Users\maryam\Desktop\politicsNews .txt");
 
-            //politicswords Data Table
-            DataTable politicsWords = new DataTable();
-            politicsWords.Columns.Add("word", typeof(string));
-            politicsWords.Columns.Add("count", typeof(int));
-
-            bool isPreposition;
-            bool isVerb;
-
-            foreach (string line in politicsLines)
-            {
-                string[] words = line.Split(new string[] { " ","!","\"","#","؛","،","-","$","%","&","'",
-                                                           "(",")","+",".","/",":",";","<",">","=","?",
-                                                           "[","]","{","}","»","«","1","2","3","4",
-                                                            "5","6","7","8","9","0","١","۲", "۳","۴",
-                                                            "۵","۶","۷","۸","۹","٠"}, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (string word in words)
-                {
-                    //Omit preposition from politics word
-                    isPreposition = false;
-                    foreach (DataRow row in preposition.Rows)
-                    {
-                        if(row.Field<string>("preposition") == word)
-                        {
-                            isPreposition = true;
-                            break;
-                        }
-                    }
-                    //Omit verb from politics word
-                    isVerb = false;
-                    foreach (DataRow row in verb.Rows)
-                    {
-                        if (row.Field<string>("verb") == word)
-                        {
-                            isVerb = true;
-                            break;
-                        }
-                    }
-                    if (isPreposition == false && isVerb == false)
-                    {
-                        //Counting the identicl word in politics News
-                        bool isrepeated = false;
-                        for(int i=0; i<politicsWords.Rows.Count; i++)
-                        {
-                            if(word == politicsWords.Rows[i]["word"].ToString())
-                            {
-                                politicsWords.Rows[i]["count"] = int.Parse(politicsWords.Rows[i]["count"].ToString()) + 1;
-                                isrepeated = true;
-                                break;
-                            }
-                        }
-                        if(isrepeated == false)
-                        {
-                            DataRow dr = politicsWords.NewRow();
-                            dr["word"] = word;
-                            dr["count"] = 1;
-                            politicsWords.Rows.Add(dr);
-                        } 
-                    }  
-                }
-            }
-
-            //sort the politics words by number of repeated
-            politicsWords.DefaultView.Sort = "count desc";
-            politicsWords = politicsWords.DefaultView.ToTable();
-
-            //Display politicsWords in dataGrid 
-            dataGridView2.DataSource = politicsWords;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            label1.Visible = false;
+            button3.Visible = true;
+            dataGridView1.Visible = true;
+            dataGridView2.Visible = true;
+            Cursor.Current = Cursors.WaitCursor;
 
             //Load sports News file
-            filePath = Path.Combine(currentDirectory, "sportsNews.txt");
-            string[] sportsLines = System.IO.File.ReadAllLines(@"C:\Users\maryam\Desktop\sportsNews.txt");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "sportsNews.txt");
+            string[] sportsLines = File.ReadAllLines(@"C:\Users\maryam\Desktop\sportsNews.txt");
 
             //sportsWords Data Table
             DataTable sportsWords = new DataTable();
             sportsWords.Columns.Add("word", typeof(string));
             sportsWords.Columns.Add("count", typeof(int));
 
+            bool isPreposition;
+            bool isVerb;
+
+            SentenceTokenizer senTokenizer = new SentenceTokenizer();
+            Normalizer normalize = new Normalizer();
+
+            DataTable sportsSentences = new DataTable();
+            sportsSentences.Columns.Add("Sentence", typeof(string));
+            sportsSentences.Columns.Add("NormalizeSentence", typeof(string));
+
             foreach (string line in sportsLines)
             {
+                string[] sentences;
+                sentences = senTokenizer.Tokenize(line).ToArray();
+                foreach (string sentence in sentences)
+                {
+                    DataRow dr = sportsSentences.NewRow();
+                    dr["Sentence"] = sentence;
+                    dr["NormalizeSentence"] = normalize.Run(sentence);
+                    sportsSentences.Rows.Add(dr);
+                }
+            }
+            dataGridView1.DataSource = sportsSentences;
+
+            foreach (string line in sportsLines)
+            {
+                //split each words of each lines
                 string[] words = line.Split(new string[] { " ","!","\"","#","؛","،","-","$","%","&","'",
                                                            "(",")","+",".","/",":",";","<",">","=","?",
                                                            "[","]","{","}","»","«","1","2","3","4",
@@ -157,17 +125,21 @@ namespace wordmap
                     }
                     //Omit verb from politics word
                     isVerb = false;
-                    foreach (DataRow row in verb.Rows)
+                    if (isPreposition == false)
                     {
-                        if (row.Field<string>("verb") == word)
+                        //Omit verb from politics word
+                        foreach (DataRow row in verb.Rows)
                         {
-                            isVerb = true;
-                            break;
+                            if (row.Field<string>("verb") == word)
+                            {
+                                isVerb = true;
+                                break;
+                            }
                         }
                     }
                     if (isPreposition == false && isVerb == false)
                     {
-                        //Counting the identicl word in sports News
+                        //Counting the identical word in sports News
                         bool isrepeated = false;
                         for (int i = 0; i < sportsWords.Rows.Count; i++)
                         {
@@ -187,16 +159,144 @@ namespace wordmap
                         }
                     }
                 }
-
             }
             //sort the sports words by number of repeated
             sportsWords.DefaultView.Sort = "count desc";
             sportsWords = sportsWords.DefaultView.ToTable();
 
             //Display sportsWords in dataGrid 
-            dataGridView3.DataSource = sportsWords;
+            dataGridView2.DataSource = sportsWords;
 
-            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            label1.Visible = false;
+            button3.Visible = true;
+            dataGridView1.Visible = true;
+            dataGridView2.Visible = true;
+            Cursor.Current = Cursors.WaitCursor;
+
+            SentenceTokenizer senTokenizer = new SentenceTokenizer();
+            Normalizer normalize = new Normalizer();
+
+            //Load politics News
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "politicsNews .txt");
+            string[] politicsLines = File.ReadAllLines(@"C:\Users\maryam\Desktop\politicsNews .txt");
+
+            //politicswords Data Table
+            DataTable politicsWords = new DataTable();
+            politicsWords.Columns.Add("word", typeof(string));
+            politicsWords.Columns.Add("count", typeof(int));
+
+
+            bool isPreposition;
+            bool isVerb;
+
+            DataTable politicsSentences = new DataTable();
+            politicsSentences.Columns.Add("Sentence", typeof(string));
+            politicsSentences.Columns.Add("NormalizeSentence", typeof(string));
+
+            foreach (string line in politicsLines)
+            {
+                string[] sentences;
+                sentences = senTokenizer.Tokenize(line).ToArray();
+                foreach (string sentence in sentences)
+                {
+                    DataRow dr = politicsSentences.NewRow();
+                    dr["Sentence"] = sentence;
+                    dr["NormalizeSentence"] = normalize.Run(sentence);
+                    politicsSentences.Rows.Add(dr);
+                }
+            }
+            dataGridView1.DataSource = politicsSentences;
+
+            foreach (string line in politicsLines)
+            {
+                string[] sentences;
+                sentences = senTokenizer.Tokenize(line).ToArray();
+                foreach (string sentence in sentences)
+                {
+                    System.Diagnostics.Debug.WriteLine(sentence);
+                }
+
+                //split each words of each lines
+                string[] words = line.Split(new string[] { " ","!","\"","#","؛","،","-","$","%","&","'",
+                                                           "(",")","+",".","/",":",";","<",">","=","?",
+                                                           "[","]","{","}","»","«","1","2","3","4",
+                                                            "5","6","7","8","9","0","١","۲", "۳","۴",
+                                                            "۵","۶","۷","۸","۹","٠"}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    //Omit preposition from politics word
+                    isPreposition = false;
+                    foreach (DataRow row in preposition.Rows)
+                    {
+                        if (row.Field<string>("preposition") == word)
+                        {
+                            isPreposition = true;
+                            break;
+                        }
+                    }
+
+                    isVerb = false;
+                    if (isPreposition == false)
+                    {
+                        //Omit verb from politics word
+                        foreach (DataRow row in verb.Rows)
+                        {
+                            if (row.Field<string>("verb") == word)
+                            {
+                                isVerb = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (isPreposition == false && isVerb == false)
+                    {
+                        //Counting the identical word in politics News
+                        bool isrepeated = false;
+                        for (int i = 0; i < politicsWords.Rows.Count; i++)
+                        {
+                            if (word == politicsWords.Rows[i]["word"].ToString())
+                            {
+                                politicsWords.Rows[i]["count"] = int.Parse(politicsWords.Rows[i]["count"].ToString()) + 1;
+                                isrepeated = true;
+                                break;
+                            }
+                        }
+                        if (isrepeated == false)
+                        {
+                            DataRow dr = politicsWords.NewRow();
+                            dr["word"] = word;
+                            dr["count"] = 1;
+                            politicsWords.Rows.Add(dr);
+                        }
+                    }
+                }
+
+            }
+            //sort the politics words by number of repeated
+            politicsWords.DefaultView.Sort = "count desc";
+            politicsWords = politicsWords.DefaultView.ToTable();
+
+            //Display politicsWords in dataGrid 
+            dataGridView2.DataSource = politicsWords;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            label1.Visible = true;
+            button3.Visible = false;
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = false;
         }
     }
 }
